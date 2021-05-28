6fcfd83f71ca357f0cc15a51387c75660ea4bdfb
ChessSuite <39228929+santafiora@users.noreply.github.com>
2021-05-28 16:39:31 +0200

    Movie Class and Json File

---
+++ Cinema/Model/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Model
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Released { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Actors { get; set; }
+        public string Poster { get; set; }
+        public string Type { get; set; }
+
+
+    }
+}
